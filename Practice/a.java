051c7c1bdc40023ff7c72b48c62a0ccf725b1087
Sookyung Kim <mikionpub@gmail.com>
2019-02-12 14:52:41 +0900

    2019.02.12

---
+++ Practice/a.java
@@ -0,0 +1,23 @@
+public class student{
+  
+  private int age;
+  private String name, gen, major;
+  
+  public void int getage{
+    return age;
+  }
+  public void int setage(int age){
+    int age = age();
+  }
+  
+  public static void main(String[] args){
+  		
+    this.age = 20;
+    this.name = "sookim";
+    this.gen = "F";
+    this.major = "Computer";
+    
+    System.out.println(age);
+    
+  }
+}
